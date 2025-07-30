5fd323da634d674054dd5838c8a559de92570095
Romyx <157620445+RomyxR@users.noreply.github.com>
2025-07-30 13:04:58 +0300

    Update b85url.py

--- b85url.py
+++ b85url.py
@@ -1,37 +1,19 @@
-import math
-
-# Base85URL алфавит (85 безопасных символов для URL)
 ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.~!*'()"
-BASE = len(ALPHABET)  # 85
+BASE = len(ALPHABET)
 
 def base85url_encode(data):
-    """Кодирует байты в Base85URL строку"""
-    if isinstance(data, str):
-        data = data.encode('utf-8')
-    # Конвертируем байты в большое число
-    num = int.from_bytes(data, byteorder='big')
-    
-    if num == 0: return ALPHABET[0]
-    # Конвертируем число в Base85
+    if isinstance(data, str): data = data.encode('utf-8')
+    num = int.from_bytes(data, 'big')
     encoded = ""
     while num > 0:
         num, remainder = divmod(num, BASE)
         encoded = ALPHABET[remainder] + encoded
-    
     return encoded
 
 def base85url_decode(encoded):
-    """Декодирует Base85URL строку в байты"""
-    num = 0
-    for char in encoded:
-        num = num * BASE + ALPHABET.index(char)
-    
-    if num == 0: return b''
-    
-    byte_length = (num.bit_length() + 7) // 8
-    return num.to_bytes(byte_length, byteorder='big')
+    num = sum(ALPHABET.index(char) * BASE**i for i, char in enumerate(reversed(encoded)))
+    return num.to_bytes((num.bit_length() + 7) // 8, 'big')
 
-# Удобные функции для строк
 def encode_string(text):
     return base85url_encode(text)
 def decode_string(encoded):
